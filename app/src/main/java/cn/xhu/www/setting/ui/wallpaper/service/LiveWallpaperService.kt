c7f2355376f07bd520052042fa27f888212bb8ed
王文 <xhuww2012@gmail.com>
2020-07-14 18:02:08 +0800

    alter LiveWallpaper code

--- app/src/main/java/cn/xhu/www/setting/ui/wallpaper/service/LiveWallpaperService.kt
+++ app/src/main/java/cn/xhu/www/setting/ui/wallpaper/service/LiveWallpaperService.kt
@@ -4,43 +4,46 @@ import android.media.MediaPlayer
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceHolder
 import cn.xhu.www.setting.ui.wallpaper.getLiveWallpaperFile
-import java.lang.Exception
 
 class LiveWallpaperService : WallpaperService() {
-    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer().apply {
+            isLooping = true
+            setVolume(0f, 0f)
+        }
+    }
 
     override fun onCreateEngine(): Engine = LiveWallpaperEngine()
 
-    inner class LiveWallpaperEngine : Engine() {
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    internal inner class LiveWallpaperEngine :
+        Engine() {
         override fun onSurfaceCreated(holder: SurfaceHolder) {
             super.onSurfaceCreated(holder)
-            val dataPath = getLiveWallpaperFile()?.absolutePath
-            mediaPlayer = MediaPlayer().apply {
-                try {
-                    setSurface(holder.surface)
-                    setDataSource(dataPath)
-                    isLooping = true
-                    setVolume(0f, 0f)
-//                    setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
-                    prepare()
-                    start()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
+            val path = getLiveWallpaperFile()?.absolutePath ?: return
+            mediaPlayer.runCatching {
+                setSurface(holder.surface)
+                setDataSource(path)
+                setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
+                setOnPreparedListener { obj: MediaPlayer -> obj.start() }
+                prepare()
             }
         }
 
         override fun onVisibilityChanged(visible: Boolean) {
             super.onVisibilityChanged(visible)
-            mediaPlayer?.run {
-                if (visible) start() else pause()
+            if (visible) {
+                mediaPlayer.start()
+            } else {
+                mediaPlayer.pause()
             }
-        }
-
-        override fun onSurfaceDestroyed(holder: SurfaceHolder?) {
-            super.onSurfaceDestroyed(holder)
-            mediaPlayer?.release()
-            mediaPlayer = null
         }
     }
 }
